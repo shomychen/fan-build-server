ea3e8229ca56f1f3d17096bbc7880b451ea37b1f
shomychen <shomychen@sina.com>
2020-08-24 14:38:06 +0800

    更新 连接mongodb数据库方法

--- server/app.js
+++ server/app.js
@@ -1,28 +1,16 @@
 const Koa = require('koa');
 const router = require('./router.js'); // 路由集合
 const dbModule = require('./utils/db.js');
+const config = require('./config.json');
 const bodyParser = require('koa-bodyparser'); // 获取post提交的数据
-const mongoose = require('mongoose');
 const app = new Koa();
 
 app.use(bodyParser()); // 转换POST请求的参数
 
-/*const db = require('./config/keys').mongoURIL
-mongoose.connect(
-  db,
-  {
-    useNewUrlParser: true
-  })
-  .then(() => {
-    console.log("连接数据库成功")
-  })
-  .catch(err => {
-    console.log(err)
-  })*/
 
-app.use(router.routes())
+app.use(router.routes()) // 引用路由
   .use(router.allowedMethods())  // 作用： 这是官方文档的推荐用法,我们可以看到router.allowedMethods()用在了路由匹配router.routes()之后,所以在当所有路由中间件最后调用.此时根据ctx.status设置response响应头
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || config.port
 app.listen(port); // 端口号监听
 console.log(`服务已启动，请打开下面链接访问: \nhttp://127.0.0.1:${port}/`);
---
+++ server/utils/db.js
@@ -0,0 +1,27 @@
+// db.js
+const mongoose = require('mongoose');
+const config = require('../config.json');
+
+// 连接数据库源
+let connectString = `mongodb://${config.db.servername}:${config.db.port}/${config.db.DATABASE}`;
+let options = {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true};
+
+mongoose.connect(connectString,options)
+
+mongoose.connection.on('connected',function() {
+  console.log('连接成功，Mongoose connection open to '+connectString);
+});
+/**
+ * 连接异常 error 数据库连接错误
+ */
+mongoose.connection.on('error',function(err) {
+  console.log('连接异常，Mongoose connection error: '+ err);
+});
+/**
+ * 连接断开 disconnected 连接异常断开
+ */
+mongoose.connection.on('disconnected',function() {
+  console.log('连接断开，Mongoose connection disconnected');
+});
+
+module.exports = mongoose
